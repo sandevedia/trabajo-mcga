d8075615756e949528722c9b88a245fb677745c0
santiago Alarcon <santiagoalarcon2@hotmail.com>
2019-11-15 20:28:14 -0300

    preparando heroku

--- src/App.js
+++ src/App.js
@@ -1,73 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Card from './card.js'
 
-class App extends React.Component {
+import{
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
 
-  state = {
-    titulo: "Mi trabajo de MCGA"
-  }
+import Login from './components/login'
+import Home from './components/home'
 
-  componentWillMount=() => {
-    console.log('se está por montar dibujar el componente')
-  }
 
-  componentDidMount=() => {
-    console.log('Ya se dibujó el componente')
-  }
-
-render(){
-  console.log('se está dibujando el componente')
-  return(
-    <div>
-
-    <button onClick={
-      () => {
-      this.setState({titulo : "Nuevo Título"})
-      }
-
-        }>
-          Cambiar título
-    </button>
-    <Card text = {this.state.titulo}></Card>
-
-    </div>
-  )
-}
-
-componentWillReceiveProps(nextProps, nextState) {
-console.log('Ya se actualizó el componente')
-console.log(nextState)
-}
-
-shouldComponentUpdate(nextProps, nextState) {
-console.log(nextState)
-return false
-}
-
-}
-
-/*
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path={'/'} component ={Login} />
+      <Route exact path={'/home'} component ={Home} />
+    </Router>
   );
 }
-*/
+
 export default App;
---
+++ src/store.js
@@ -0,0 +1,125 @@
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
+
+const initialState = {
+  email: undefined,
+  password: undefined,
+  logged: false,
+  message: undefined,
+  isLogging: false
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ON_CHANGE_EMAIL':
+      return {
+        ...state,
+        email: action.payload,
+        logged: false,
+        message: undefined
+      }
+    case 'ON_CHANGE_PASSWORD':
+      return {
+        ...state,
+        password: action.payload,
+        logged: false,
+        message: undefined
+      }
+
+    case 'loggin_success':
+      return{
+        ...state,
+        isLogging: false,
+        message: action.payload.message,
+        logged: action.payload.success,
+      }
+
+      case 'loggin_error':
+        return{
+          ...state,
+          isLogging: false,
+          message: action.payload.message,
+          logged: action.payload.success,
+        }
+
+
+      case 'login_pending':
+        return{
+          ...state,
+          isLogging: true,
+        }
+
+    default:
+      return state
+  }
+}
+
+export const onChangeEmail = (event) => {
+  const text = event.target.value
+  return {
+    type: 'ON_CHANGE_EMAIL',
+    payload: text
+  }
+}
+
+export const onChangePassword = (event) => {
+  const text = event.target.value
+  return {
+    type: 'ON_CHANGE_PASSWORD',
+    payload: text
+  }
+}
+
+
+export const handleLogin = (email, password) => {
+  return (dispatch) =>{
+    dispatch({
+      type: "login_pending"
+    })
+
+    return fetch ("http://localhost:4000/login", {
+      baseURL: "http://localhost:4000",
+      timeout: 25000,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify ({
+        email: email,
+        password: password
+      })
+    })
+    .then(a => a.json())
+    .then((data) =>{
+      if(!data.success){
+        return dispatch({
+          type:"loggine_error",
+          payload:data,
+        })
+      }
+      return dispatch({
+        type:"login_success",
+        payload: data,
+      })
+      console.log("termino la promesa");
+    })
+    .catch((error) =>{
+      return dispatch({
+        type:"loggine_error",
+        payload:error,
+      })
+    })
+
+  }
+}
+
+
+
+const middleware = [thunk]
+
+const store = createStore(reducer, composeWithDevTools(
+  applyMiddleware(...middleware),
+));
+
+export default store
